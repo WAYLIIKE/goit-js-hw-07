14e1ff11754e3abcb9e10b9d9bfa64c025fcb981
Valentyn Savytskyi <valentynsavitskyy@gmail.com>
2023-12-09 21:56:25 +0100

    Complete 2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function galleryMarkup(arr) {
+  const markup = arr.map(({ original, preview, description }) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    li.classList.add("gallery__item");
+    a.classList.add("gallery__link");
+    a.href = original;
+    img.classList.add("gallery__image");
+    img.dataset.source = original;
+    img.alt = description;
+    img.src = preview;
+    a.append(img);
+    li.append(a);
+    return li;
+  });
+  galleryContainer.append(...markup);
+}
+
+galleryMarkup(galleryItems);
+
+galleryContainer.addEventListener("click", handleLink);
+
+function handleLink(event) {
+  event.preventDefault();
+
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  //Finding clicked photo
+  const currentPhoto = event.target.closest(".gallery__image");
+  //Finding clicked photo value of data attribute
+  const galleryElement = currentPhoto.dataset.source;
+  //Getting current photo object
+  const photo = galleryItems.find(
+    ({ original }) => galleryElement === original
+  );
+
+  const instance = basicLightbox.create(`
+          <img src="${photo.original}" alt="${photo.description}">
+    `);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function galleryMarkup(arr) {
+  const markup = arr.map(({ original, preview, description }) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    li.classList.add("gallery__item");
+    a.classList.add("gallery__link");
+    a.href = original;
+    img.classList.add("gallery__image");
+    img.alt = description;
+    img.src = preview;
+    a.append(img);
+    li.append(a);
+    return li;
+  });
+  galleryContainer.append(...markup);
+}
+
+galleryMarkup(galleryItems);
+
+let lightBox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
